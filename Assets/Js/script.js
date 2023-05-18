f5ccfd5a55dae122f7fb8dff0bba6e6cfb508cd8
Emily <85910442+EmilySSouza@users.noreply.github.com>
2023-05-18 20:37:16 -0300

    Ajustes no Script

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,14 +1,11 @@
-// Pega o checkbox
 const botaoDark = document.getElementById('toggle-dark');
 
-// Verifica se tem no localStorage seleção do dark theme
 if (localStorage.getItem('data-theme') == 'dark') {
   botaoDark.checked = true;
 }
 
-// Liga a função ao checkbox
 botaoDark.addEventListener('change', () => {
-    let theme = localStorage.getItem('data-theme'); // Retrieve saved them from local storage
+    let theme = localStorage.getItem('data-theme');
     if (!botaoDark.checked){
         changeThemeToLight()
     }else{
